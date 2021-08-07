68933fc7ab4d0d9904ceeb097e2415bf6adf8539
Daiigr <danielgurbac@gmail.com>
2021-08-08 01:53:28 +0300

    added discord reponce system

--- src/main/java/Daiigr/com/Bot.java
+++ src/main/java/Daiigr/com/Bot.java
@@ -1,11 +1,13 @@
 package Daiigr.com;
 
 import java.awt.Color;
+import java.io.IOException;
+
+import javax.swing.event.SwingPropertyChangeSupport;
+
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import org.json.*;
 
 import Daiigr.Config;
@@ -24,51 +26,73 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 public class Bot extends ListenerAdapter{
 
     private static Config config;
-    private static String[] temp;
-    private static FileList nonUser;
-    private static FileList nonWord;
+    private String temp;
+
         
 public static void main(String[] args) throws Exception{
     try {
-       // config = new Config("config.properties");
-       // JDABuilder.createDefault(config.getToken())
-       // .addEventListeners(new Bot())
-       // .setActivity(Activity.playing("Anna Simulator(v.2)"))
-       // .build();
+       config = new Config("config.properties");
+        JDABuilder.createDefault(config.getToken())
+        .addEventListeners(new Bot())
+        .setActivity(Activity.playing("test"))
+        .build();
 
      
 
-OkHttpClient client = new OkHttpClient();
-
-Request request = new Request.Builder()
-   .url("https://www.boredapi.com/api/activity?type=social")
-   .build(); // defaults to GET
-
-Response response = client.newCall(request).execute();
-
-String jsonString = response.body().string();
-JSONObject obj = new JSONObject(jsonString);
-String activity = obj.getString("activity");
-System.out.println(activity);
 
     } catch (Exception e) {
         e.printStackTrace();
-       // System.out.print("invalid Token: " + config.getToken());
+      
     }
 
 }
-
     @Override
     public void onMessageReceived(MessageReceivedEvent event){
 
         Message msg = event.getMessage();
-       temp = msg.getContentRaw().toLowerCase().split(" ");   
+
+       temp = msg.getContentRaw().toLowerCase(); 
+
+       if(temp.toLowerCase().equals("-activity")){
+
+        MessageChannel channel = event.getChannel();
+
+        try {
+
+            channel.sendMessage(getActivity()).queue();
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+       }  
 
       
 
 
         
               }      
+
+              public String getActivity() throws IOException{
+
+                OkHttpClient client = new OkHttpClient();
+
+                Request request = new Request.Builder()
+
+                   .url("https://www.boredapi.com/api/activity")
+                   .build(); // defaults to GET
+                
+                Response response = client.newCall(request).execute();
+                
+                String jsonString = response.body().string();
+
+                JSONObject obj = new JSONObject(jsonString);
+                String activity = obj.getString("activity");
+                System.out.println(activity);
+                
+            
+                  return activity;
+              }
     }
 
     
